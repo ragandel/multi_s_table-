88c9d610d7784b2c2732f57749c41e863dfff67d
ragandel <32456198+ragandel@users.noreply.github.com>
2017-10-06 15:21:17 +0300

    edit file multu_sort.js

--- multi_sort.js
+++ multi_sort.js
@@ -1,20 +1,19 @@
 function SortTable (id_table, arrNotSortCells){
-	"use strict"
+	"use strict";
 	var config = {
-		nameClass : '.sort-table',                     //название класса таблицы для сортировки
-		regExp :{
-			_ip : /[1-9][0-9]{0,2}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/,
-			_time : /[0-2][0-9]\:[0-5][0-9]\:[0-5][0-9]/,
-			_date : /[1-9][0-9]{3}\-[0-1][0-9]\-[0-3][0-9]/,
+		nameClass: '.sort-table',                     //название класса таблицы для сортировки
+		regExp: {
+			_ip: /[1-9][0-9]{0,2}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/,
+			_time: /[0-2][0-9]\:[0-5][0-9]\:[0-5][0-9]/,
+			_date: /[1-9][0-9]{3}\-[0-1][0-9]\-[0-3][0-9]/,
 			_date_time: /[1-9][0-9]{3}\-[0-1][0-9]\-[0-3][0-9]\s[0-2][0-9]\:[0-5][0-9]\:[0-5][0-9]/,
-			_time_date : /[0-2][0-9]\:[0-5][0-9]\:[0-5][0-9]\s[1-9][0-9]{3}\-[0-1][0-9]\-[0-3][0-9]/
+			_time_date: /[0-2][0-9]\:[0-5][0-9]\:[0-5][0-9]\s[1-9][0-9]{3}\-[0-1][0-9]\-[0-3][0-9]/
 		},
 		_span_block : {
-			_span_arrow : "<span class='rows'> <span class='up'> &#9650 </span> <span class='down'> &#9660 </span> </span>",
-			_span_tTip : "<span class='tooltiptext'> Столбец пустой </span>"
+			_span_arrow: "<span class='rows'> <span class='up'> &#9650 </span> <span class='down'> &#9660 </span> </span>",
+			_span_tTip: "<span class='tooltiptext'> Столбец пустой </span>"
 		}
 	}
-        
         var namesTab = id_table.concat(config.nameClass);     //массив содержащий ID и класс сортируемых табли
 	var fSort = 1;                                        //направление сортировки
 	var indCell;                                          //индекс сортируемого столбца
@@ -22,20 +21,22 @@ function SortTable (id_table, arrNotSortCells){
 	
 	function toolTip(){
 		//удаление подсказки
-                var tr_head = $(this).parent();
+                let tr_head = $(this).parent();
 		tr_head.find('.tooltiptext').remove();
 		tr_head.find('.tool_tip').removeClass('tool_tip');
 	}
 	
 	function trSort(arrData, arrObj, index) {
 		//функция возвращает отсортированный объект строк
-	        var count = arrObj.length;
-		for (var i = 0; i < count; i ++){
-			for (var j = 0; j < count -1; j++){
+	        let  count = arrObj.length;
+		let  arrEnd = [];
+		
+		for (let i = 0; i < count; ++i){
+			for (let j = 0; j < count -1; ++j){
 				if ((fSort == 1 && arrData[j] > arrData[j+1]) ||
 					(fSort != 1) && arrData[j] < arrData[j+1]) {
-					var item_1;
-					var item_2;
+					let item_1;
+					let item_2;
 					
 					item_1 = arrData[j];
 					arrData[j] = arrData[j+1];
@@ -47,8 +48,6 @@ function SortTable (id_table, arrNotSortCells){
 			}
 		}
 		
-		var arrEnd = [];
-		
                 index.forEach(function(item){
                         //добавляем отсортированные строки таблицы в новый массив
 	                arrEnd.push(arrObj[item]);
@@ -57,12 +56,11 @@ function SortTable (id_table, arrNotSortCells){
 	}
  
 	function selectData(){
-		var arrTr =[];                  //масиив строк tr текущей таблицы
-		var value;                      //значение ячейки
-		var empty = 0;                  //показатель пустых ячеек
-		var notSortFlag = 0;            //флаг отмены сортировки
-		
-	        var _arr = {
+		let arrTr = [];                 //масиив строк tr текущей таблицы
+		let value;                      //значение ячейки
+		let empty = 0;                  //показатель пустых ячеек
+		let notSortFlag = 0;            //флаг отмены сортировки
+	        let _arr = {
                         _index: {
                         	_ABC: [],
                                 _Number: [],    //индексы строк с числовыми данными
@@ -77,28 +75,30 @@ function SortTable (id_table, arrNotSortCells){
                                 _date: []       //массив  для хранения даты в ms
                         },
 		        _name_domen:{
-                                _http : 'http://',
-			        _https : 'https://'
+                                _http: 'http://',
+			        _https: 'https://'
 		        }
                 }
+                
 		//массив объектов строк
 		arrTr = $(id_T).find('tbody tr');
-		//метод формирования алфавита IP, Date, Domen, string, number, empty
+		
 		function genAlp(value,i){
+			let valueStore;
+			
+			//метод формирования алфавита IP, Date, Domen, string, number, empty
 			if(value.length <= 0){
 				//формирование алфавита пустых ячееек
-				empty++;                             //счетчик пустых ячеек
+				++empty;                             //счетчик пустых ячеек
 				_arr._index._Space.push(i);          //индекс пустых ячеек
 				return;
 			}
 			
-			var valueStore;
-			
 			if ((isNaN(value))) {
 				if( valueStore = value.match(config.regExp._ip)) {
 					//формирование алфавита IP
-					var IP = String(valueStore).split('.');
-					var hashIP = (IP[0] * 16777216)  + (IP[1]*65536)  + (IP[2] * 256) + IP[3]*1;
+					let IP = String(valueStore).split('.');
+					let hashIP = (IP[0]*16777216)  + (IP[1]*65536)  + (IP[2]*256) + IP[3]*1;
 					_arr._data._ip.push(hashIP);
 					_arr._index._IP.push(i);
 				}else if(value.match(config.regExp._time) ||
@@ -118,8 +118,8 @@ function SortTable (id_table, arrNotSortCells){
 	                                        _arr._index._date.push(i);
                                         }else if(valueStore = value.match(config.regExp._time)){
 	                                        //если формат " **:**:** "
-	                                        var elDate = String(valueStore).split(':');
-	                                        var storDate = elDate[0] * 100 + elDate[1] * 10 + elDate[2] + Date.now();
+	                                        let elDate = String(valueStore).split(':');
+	                                        let storDate = elDate[0]*100 + elDate[1]*10 + elDate[2] + Date.now();
 	                                        _arr._data._date.push(storDate);
 	                                        _arr._index._date.push(i);
                                         }
@@ -144,15 +144,15 @@ function SortTable (id_table, arrNotSortCells){
 		}
 		
 		$.each(arrTr, function(i){
-			var val = $(this).find('td,th').eq(indCell);
+			let val = $(this).find('td,th').eq(indCell);
 			if ($(val).find('input').length > 0){
 				//выборка input  - text
-				var el_Input = $(val).find('input');
+				let el_Input = $(val).find('input');
 				if (el_Input.filter(':text').length > 0){
 					value = el_Input.filter(':text').val().trim();
 					genAlp(value,i);
 				} else if(el_Input.filter(':hidden').length > 0) {
-					empty++;                                        //счетчик пустых ячеек
+					++empty;                                        //счетчик пустых ячеек
 					_arr._index._Space.push(i);                     //индекс пустых ячеек
 				}else if(el_Input.filter(':checkbox').length > 0) {
 					value = el_Input.filter(':checkbox');
@@ -160,7 +160,7 @@ function SortTable (id_table, arrNotSortCells){
 						_arr._data._number.push(1);
 						_arr._index._Number.push(i);
 					}else {
-						empty++;                                 //счетчик пустых ячеек
+						++empty;                                 //счетчик пустых ячеек
 						_arr._index._Space.push(i);              //индекс пустых ячеек
 					}
 				}
@@ -178,62 +178,58 @@ function SortTable (id_table, arrNotSortCells){
 			}
 		})
 		
-		if (empty != arrTr.length){
-			//если столбец не пустой
-		        //очищаем DOM от предыдущей таблицы
-			var tbodyNew = $(id_T).find('tbody').empty();
-			var _arr_data_ind = [
-						{data : _arr._data._number, index : _arr._index._Number},
-						{data : _arr._data._date, index : _arr._index._date},
-						{data : _arr._data._ip, index : _arr._index._IP},
-						{data : _arr._data._abc, index : _arr._index._ABC}
-					    ];
-			if (fSort==1){
-				if (_arr._index._Space.length){
-					//добавление алфавита пустых ячеек
-					_arr._index._Space.forEach(function(item, i,arr){tbodyNew.append(arrTr[item]);});
-				};
-				_arr_data_ind.forEach(function(item, i){
-					if(item.data.length > 0)
-						tbodyNew.append(trSort(item.data, arrTr, item.index));
-				});
-			}else {
-				_arr_data_ind.forEach(function(item, index){
-					if(_arr_data_ind[_arr_data_ind.length - index - 1].data.length > 0)
-						tbodyNew.append(trSort(_arr_data_ind[_arr_data_ind.length - index -1].data,
-							         arrTr,_arr_data_ind[_arr_data_ind.length - index -1].index));
-				});
-				if (_arr._index._Space.length){
-					_arr._index._Space.forEach(function(item, i,arr){tbodyNew.append(arrTr[item]);});
-				}
-			}
-		}else {
+		if (empty == arrTr.length){
 			notSortFlag = 1;
+			return;
+		}
+		//если столбец не пустой
+		//очищаем DOM от предыдущей таблицы
+		let tbodyNew = $(id_T).find('tbody').empty();
+		let _arr_data_ind = [
+			{data: _arr._data._number, index: _arr._index._Number},
+			{data: _arr._data._date, index: _arr._index._date},
+			{data: _arr._data._ip, index: _arr._index._IP},
+			{data: _arr._data._abc, index: _arr._index._ABC}
+		];
+		if (fSort == 1){
+			if (_arr._index._Space.length){
+				//добавление алфавита пустых ячеек
+				_arr._index._Space.forEach(function(item, i, arr){tbodyNew.append(arrTr[item]);});
+			};
+			_arr_data_ind.forEach(function(item, i){
+				if(item.data.length > 0)
+					tbodyNew.append(trSort(item.data, arrTr, item.index));
+			});
+		}else {
+			_arr_data_ind.forEach(function(item, index){
+				if(_arr_data_ind[_arr_data_ind.length - index - 1].data.length > 0)
+					tbodyNew.append(trSort(_arr_data_ind[_arr_data_ind.length - index - 1].data,
+						arrTr,_arr_data_ind[_arr_data_ind.length - index - 1].index));
+			});
+			if (_arr._index._Space.length){
+				_arr._index._Space.forEach(function(item, i, arr){tbodyNew.append(arrTr[item]);});
+			}
 		}
 		return notSortFlag;
 	}
-	//стартовая прорисовка указателей сортировки
+	
 	if(namesTab.length > 0){
+		//стартовая прорисовка указателей сортировки
 		namesTab.forEach(function(item){
 			$(item + ' thead').find('.rows').remove();
-			$(config._span_block._span_arrow).appendTo($(item + ' thead').find('th,td'));
+			$(config._span_block._span_arrow).appendTo($(item + ' thead').find('th, td'));
 		})
 	}
 	
-	var cellThead = $(namesTab.join(',')).find('th');       //все загловки th сортируемых таблиц
-	
-        cellThead.click(function() {
-        	var _this_table = $(this).parents('table');     //записываем имя ID или CLass сортируемой таблицы
-	        
-                if (_this_table.attr('id'))
-                        id_T = '#' + _this_table.attr('id');
-                else if (_this_table.attr('class'))
-                        id_T = '.' + _this_table.attr('class');
-                
+	$(namesTab.join(',')).find('th').click(function() {
+        	let _this_table = $(this).parents('table');     //записываем имя ID или CLass сортируемой таблицы
+		
+		id_T = (_this_table.attr('id')) ?  ('#' + _this_table.attr('id')) : ('.' + _this_table.attr('class'));
+		
                 indCell = $(this).index();
                 
                 if (arrNotSortCells.indexOf(indCell) == -1){    //проверка что  в arrNotSortCells нет indCell
-                        var notSortFlag = selectData();         //1 - столбец пустой, иначе 0
+                        let notSortFlag = selectData();         //1 - столбец пустой, иначе 0
                         
 	                if(notSortFlag){
 		                //проверка индекса столбца на сортировку
@@ -241,19 +237,20 @@ function SortTable (id_table, arrNotSortCells){
 		                $(this).addClass('tool_tip');
 		                $(config._span_block._span_tTip).appendTo($(this));
 		                setTimeout(toolTip.bind(this), 1000);
-		                $(id_T).find('.up, .down').css('visibility','visible');
+		                $(id_T).find('.up, .down').css('visibility', 'visible');
 		                return
 	                }
 	                //восстанавливаем исходные стрелки и очищаем таблицу от данного id таблицы
-	                $(id_T).find('.up, .down').css('visibility','visible');
+	                $(id_T).find('.up, .down').css('visibility', 'visible');
 	                
-	                var _css_arrow = (fSort) ? 'visible' : 'hidden';
-	                var _css_not_arrow = (fSort) ? 'hidden' : 'visible';
+	                let _css_arrow = (fSort) ? 'visible' : 'hidden';
+	                let _css_not_arrow = (fSort) ? 'hidden' : 'visible';
 		
-	                $(id_T).find('.down').eq(indCell).css('visibility',_css_not_arrow);
-	                $(id_T).find('.up').eq(indCell).css('visibility',_css_arrow);
+	                $(id_T).find('.down').eq(indCell).css('visibility', _css_not_arrow);
+	                $(id_T).find('.up').eq(indCell).css('visibility', _css_arrow);
 		
 	                fSort ^= 1;
                 }
         });
 }
+
